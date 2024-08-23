6c6e8b3eb2deea72fe6fd5f14e1a6053ec8d3e1b
khjun128 <khj10283@naver.com>
2024-08-24 08:29:25 +0900

    8080 포트로 변경

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(80);
+  await app.listen(8080);
 }
 bootstrap();

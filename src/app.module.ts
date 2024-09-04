1a4a80198aefbaa20c33176f4c5fe76785673120
khjun128 <khj10283@naver.com>
2024-09-05 04:56:45 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { typeOrmConfig } from './config/ormconfig';
-import { JwtAuthGuard } from './utils/guards/jwt-auth.guard';
-import { TransactionCoreModule } from './transaction-core/transaction-core.module';
-import { StudyCreateModule } from './study-create/study-create.module';
-<<<<<<< HEAD
 import { AwsS3Module } from './aws-s3/aws-s3.module';
-=======
+import { typeOrmConfig } from './config/ormconfig';
 import { FirebaseModule } from './firebase/firebase.module';
->>>>>>> origin/fcm
+import { StudyCreateModule } from './study-create/study-create.module';
+import { TransactionCoreModule } from './transaction-core/transaction-core.module';
+import { JwtAuthGuard } from './utils/guards/jwt-auth.guard';
 
 @Module({
   imports: [
@@ -25,11 +22,8 @@ import { FirebaseModule } from './firebase/firebase.module';
     AuthModule,
     TransactionCoreModule,
     StudyCreateModule,
-<<<<<<< HEAD
     AwsS3Module,
-=======
     FirebaseModule,
->>>>>>> origin/fcm
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: JwtAuthGuard }],
